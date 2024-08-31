ffc486ca806af8ac69e48283aa7a1bdab62c4918
Max <maxhard243@gmail.com>
2024-09-01 01:35:37 +0300

    style

--- src/app/routers/stylex.module.ts
+++ src/app/routers/stylex.module.ts
@@ -5,5 +5,9 @@ export const styles = stylex.create({
     background: 'white',
     padding: '0 0 92.5px 0',
     width: '100%',
+    margin: '0 auto',
+    fontFamily: 'Sf-pro-rounded, sans-seri',
+    maxWidth: '1000px',
+    position: 'relative',
   }
 })
