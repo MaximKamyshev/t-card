6b0cede27918787632ec79c2660df2d14bfbec4e
Max <maxhard243@gmail.com>
2024-09-01 01:53:50 +0300

    style

--- src/app/routers/stylex.module.ts
+++ src/app/routers/stylex.module.ts
@@ -3,7 +3,7 @@ import * as stylex from '@stylexjs/stylex';
 export const styles = stylex.create({
   wrapper: {
     background: 'white',
-    padding: '0 0 92.5px 0',
-    width: '100%',
+    padding: '0 0 68.5px 0',
+    width: '100px',
   }
 })
