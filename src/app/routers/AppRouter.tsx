90df992959367df0387c841ad58d79b3af16413f
Max <maxhard243@gmail.com>
2024-09-01 04:22:24 +0300

    style

--- src/app/routers/AppRouter.tsx
+++ src/app/routers/AppRouter.tsx
@@ -40,7 +40,7 @@ const navItems = [
 export const AppRouter: React.FC = () => {
   postEvent('web_app_set_background_color', {color: '#ffffff'});
   const [swipeBehavior] = initSwipeBehavior()
-  // swipeBehavior.disableVerticalSwipe();
+  swipeBehavior.disableVerticalSwipe();
   const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
   const [location, reactNavigator] = useIntegration(navigator);
 
