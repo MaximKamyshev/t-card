825ef7fb4fceb63752418b9cbbc7c24dac05af64
Max <maxhard243@gmail.com>
2024-09-01 04:17:02 +0300

    refactor router

--- src/app/routers/AppRouter.tsx
+++ src/app/routers/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
 import { HomePage } from "../../pages/home-page";
 import { JobsPage } from "../../pages/jobs-page";
 import { NavBar } from "../../widgets/nav-bar";
@@ -8,9 +8,11 @@ import JobsLogo from '../../shared/images/for-button-with-subscription/jobsLogo.
 import ProfileLogo from '../../shared/images/for-button-with-subscription/profileLogo.svg';
 import { Loading } from "../../widgets/loading";
 import { postEvent } from "@telegram-apps/sdk";
-import { initSwipeBehavior } from "@telegram-apps/sdk-react";
+import { initNavigator, initSwipeBehavior } from "@telegram-apps/sdk-react";
 import * as stylex from '@stylexjs/stylex';
 import { styles } from './stylex.module';
+import { useEffect, useMemo } from "react";
+import { useIntegration } from "@telegram-apps/react-router-integration";
 
 const navItems = [
   {
@@ -39,55 +41,48 @@ export const AppRouter: React.FC = () => {
   postEvent('web_app_set_background_color', {color: '#ffffff'});
   const [swipeBehavior] = initSwipeBehavior()
   swipeBehavior.disableVerticalSwipe();
-  const router = createBrowserRouter([
-    {
-      path: '/home',
-      element: (
-      <>
-        <HomePage />
-        <NavBar items={navItems} />
-      </>
-      ),
-    },
-    {
-      path: '/bookmark',
-      element: (
-        <>
-          <HomePage />
-          <NavBar items={navItems} />
-        </>
-      ),
-    },
-    {
-      path: '/jobs',
-      element: (
-        <>
-          <JobsPage />
-          <NavBar items={navItems} />
-        </>
-      ),
-    },
-    {
-      path: '/profile',
-      element: (
-        <>
-          <HomePage />
-          <NavBar items={navItems} />
-        </>
-      ),
-    },
-    {
-      path: '/*',
-      element: (
-        <Navigate to="/home" />
-      ),
-    }
-  ]);
+  const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
+  const [location, reactNavigator] = useIntegration(navigator);
+
+  useEffect(() => {
+    navigator.attach();
+    return () => navigator.detach();
+  }, [navigator]);
 
   return (
     <div {...stylex.props(styles.wrapper)}>
       <Loading />
-      <RouterProvider router={router} />
+      <Router location={location} navigator={reactNavigator}>
+      <Routes>
+        <Route path={'/home'} element={
+          <>
+            <HomePage/>
+            <NavBar items={navItems} />
+          </>
+        }/>
+        <Route path={'/bookmark'} element={
+          <>
+            <HomePage/>
+            <NavBar items={navItems} />
+          </>
+        }/>
+        <Route path={'/jobs'} element={
+          <>
+            <JobsPage/>
+            <NavBar items={navItems} />
+          </>
+        }/>
+        <Route path={'/profile'} element={
+          <>
+            <HomePage/>
+            <NavBar items={navItems} />
+          </>
+        }/>
+        <Route path={'/*'} element={
+          <Navigate to='/home' />
+        }/>
+      </Routes>
+    </Router>
     </div>
   )
 }
