c421868294b16f10900e13383434d418e883866c
Max Kamyshev <maxhard243@gmail.com>
2024-09-08 17:28:16 +0300

    style

--- src/pages/profile-page/ui/ProfilePage.tsx
+++ src/pages/profile-page/ui/ProfilePage.tsx
@@ -6,7 +6,7 @@ import { CustomButton } from "../../../shared/ui/custom-button";
 import checklistLogo from '../images/checklist.svg';
 import linkLogo from '../images/link.svg';
 import { Link } from "react-router-dom";
-// import messageNotifLogo from '../images/messageNotif.svg';
+import messageNotifLogo from '../images/messageNotif.svg';
 import createResumeImage from '../images/createResume.png';
 import { TestVersionText } from "../../../shared/ui/test-version-text/TestVersionText";
 import { useInvitesModalStore } from "../../../app/stores/invites-modal/invites-modal.store";
@@ -27,13 +27,13 @@ export const ProfilePage: React.FC = () => {
       <p {...stylex.props(styles.name)}>{firstName} {lastName}</p>
       <p {...stylex.props(styles.ivites)}>Invited friends: 18</p>
       <div {...stylex.props(styles.buttonsContainer)}>
-        <CustomButton value="Tasks" bgColor="#00A77F" px={15} py={12.5} imageUrl={checklistLogo} imageSize={24} />
+        <CustomButton value="Tasks" bgColor="#00A77F" px={15} py={12.5} imageUrl={messageNotifLogo} imageSize={24} />
         <CustomButton handlePress={handleOpenBottomSheet} value="Invite Friend" px={15} py={12.5} imageUrl={linkLogo} imageSize={20.5} />
         <CustomButton value="Rewards" isDisabled={true} bgColor="#0088FF" px={15} py={12.5} />
       </div>
       <p {...stylex.props(styles.description)}>By completing tasks and inviting friends you will receive TLS coins, which can be spent in the Rewards section, and in the future you can use them to pay in the app and participate in airdrop.</p>
       <div {...stylex.props(styles.problemsCardContainer)}>
-        <span {...stylex.props(styles.logo(linkLogo, 24))} />
+        <span {...stylex.props(styles.logo(checklistLogo, 24))} />
         <p {...stylex.props(styles.notifText)}>Have you found a problem with the app or have ideas for improvement?</p>
         <div {...stylex.props(styles.felxBetweenContainer)}>
           <Link {...stylex.props(styles.notifLink)} to="#">Let us know</Link>
