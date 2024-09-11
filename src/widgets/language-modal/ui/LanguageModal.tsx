d8d525b8024aa95c9c0ee0f0c865bce8ae463b1d
Max Kamyshev <maxhard243@gmail.com>
2024-09-11 19:40:32 +0300

    style

--- src/widgets/language-modal/ui/LanguageModal.tsx
+++ src/widgets/language-modal/ui/LanguageModal.tsx
@@ -52,7 +52,7 @@ export const LanguageModal: React.FC = () => {
             setCheckedValue(user.languageCode)
           }
           else {
-            setCheckedValue(value)
+            setCheckedValue(user.languageCode)
           }
 
         })
